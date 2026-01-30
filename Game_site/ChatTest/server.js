3f3765b4703807749b7288a279b3574beb55580a
YTRellVr <R4llalt@gmail.com>
2026-01-30 13:46:40 -0500

    Add files via upload

---
+++ Game_site/ChatTest/server.js
@@ -0,0 +1,9 @@
+﻿// server.js
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.post('/messages', (req, res) => {
+  console.log('received', req.body);
+  res.json({ ok: true });
+});
+app.listen(3000, () => console.log('listening on http://localhost:3000'));
---
+++ Game_site/ChatTest/ws-server.js
@@ -0,0 +1,9 @@
+﻿const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 3001 });
+wss.on('connection', ws => {
+  ws.on('message', msg => {
+    // broadcast to others
+    wss.clients.forEach(c => c.readyState === WebSocket.OPEN && c.send(msg));
+  });
+});
+console.log('ws listening ws://localhost:3001');
