5c039ec3f26878e1b19dc9ac010e594c338896ac
YTRellVr <R4llalt@gmail.com>
2026-01-30 16:46:21 -0500

    Delete Game_site/ChatTest/server.js

--- Game_site/ChatTest/server.js
+++
@@ -1,9 +0,0 @@
-﻿// server.js
-const express = require('express');
-const app = express();
-app.use(express.json());
-app.post('/messages', (req, res) => {
-  console.log('received', req.body);
-  res.json({ ok: true });
-});
-app.listen(3000, () => console.log('listening on http://localhost:3000'));
