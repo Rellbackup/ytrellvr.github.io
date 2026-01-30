bfa7263033f59a52816a3f75ec0381259d2f8004
YTRellVr <R4llalt@gmail.com>
2026-01-30 16:46:54 -0500

    Delete Game_site/ChatTest/ws-server.js

--- Game_site/ChatTest/ws-server.js
+++
@@ -1,9 +0,0 @@
-﻿const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 3001 });
-wss.on('connection', ws => {
-  ws.on('message', msg => {
-    // broadcast to others
-    wss.clients.forEach(c => c.readyState === WebSocket.OPEN && c.send(msg));
-  });
-});
-console.log('ws listening ws://localhost:3001');
